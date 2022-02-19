aa2eda57e6b372ad6ac4a597222a012d1531fb99
rain <20520@126.com>
2022-02-19 21:26:44 +0800

    show hello world

--- src/boot.asm
+++ src/boot.asm
@@ -15,6 +15,17 @@ mov sp, 0x7c00
 mov ax, 0xb800
 mov ds, ax
 mov byte [0], 'H'
+mov byte [2], 'e'
+mov byte [4], 'l'
+mov byte [6], 'l'
+mov byte [8], 'o'
+mov byte [10], ','
+mov byte [12], 'w'
+mov byte [14], 'o'
+mov byte [16], 'r'
+mov byte [18], 'l'
+mov byte [20], 'd'
+mov byte [22], '!'
 
 ; 阻塞
 jmp $
