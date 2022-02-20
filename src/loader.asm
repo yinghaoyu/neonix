3bf93ca2556a15198c9fb709f0028607ce5c73bb
rain <20520@126.com>
2022-02-20 10:11:15 +0800

    loader.asm first added

---
+++ src/loader.asm
@@ -0,0 +1,26 @@
+[org 0x1000]
+
+dw 0x55aa; 魔数，用于判断错误
+
+; 打印字符串
+mov si, loading
+call print
+
+jmp $
+
+; xchg bx, bx; 断点
+
+print:
+    mov ah, 0x0e
+.next:
+    mov al, [si]
+    cmp al, 0
+    jz .done
+    int 0x10
+    inc si
+    jmp .next
+.done:
+    ret
+
+loading:
+    db "Loading Neonix...", 10, 13, 0; \n\r
