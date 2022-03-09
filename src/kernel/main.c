efbd6fb4524ca18f6b351b12d0147431923df81f
rain <20520@126.com>
2022-03-09 10:44:32 +0800

    clock timer pcs

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,38 +1,19 @@
-#include <neonix/neonix.h>
-#include <neonix/types.h>
-#include <neonix/io.h>
-#include <neonix/string.h>
-#include <neonix/task.h>
-#include <neonix/console.h>
-#include <neonix/printk.h>
-#include <neonix/global.h>
-#include <neonix/debug.h>
-#include <neonix/interrupt.h>
-#include <neonix/assert.h>
-#include <neonix/stdlib.h>
-
-char message[] = "hello world!\n";
+extern void console_init();
+extern void gdt_init();
+extern void interrupt_init();
+extern void clock_init();
+extern void time_init();
+extern void task_init();
+extern void hang();
 
 void kernel_init()
 {
   console_init();
-  //int cnt = 30;
-  //while(cnt--)
-  //{
-  //  printk("hello neonix %#010x\n", cnt);
-  //}
   gdt_init();
   interrupt_init();
-  task_init();
-  //assert(3 > 5);
-  //asm volatile("sti\n");
-
-  //u32 counter = 0;
-  //while(true)
-  //{
-  //  DEBUGK("looping in kernel_init %d...\n", counter++);
-  //  delay(100000000);
-  //}
-
+  clock_init();
+  time_init();
+  //task_init();
+  asm volatile("sti\n");
   return;
 }
