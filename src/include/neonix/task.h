4118d8841b43db68814101a6dad161085e4fe920
rain <20520@126.com>
2022-05-04 12:38:41 +0800

    add kernel task

--- src/include/neonix/task.h
+++ src/include/neonix/task.h
@@ -2,23 +2,58 @@
 #define NEONIX_TASK_H
 
 #include <neonix/types.h>
+#include <neonix/list.h>
+
+#define KERNEL_USER 0
+#define NORMAL_USER 1
+
+#define TASK_NAME_LEN 16
 
 typedef u32 target_t();
 
+typedef enum task_state_t
+{
+  TASK_INIT,     // 初始化
+  TASK_RUNNING,  // 执行
+  TASK_READY,    // 就绪
+  TASK_BLOCKED,  // 阻塞
+  TASK_SLEEPING, // 睡眠
+  TASK_WAITING,  // 等待
+  TASK_DIED,     // 死亡
+} task_state_t;
+
 typedef struct task_t
 {
-    u32 *stack; // 内核栈
+  u32 *stack; // 内核栈
+  list_node_t node;         // 任务阻塞节点
+  task_state_t state;       // 任务状态
+  u32 priority;             // 任务优先级
+  u32 ticks;                // 剩余时间片
+  u32 jiffies;              // 上次执行时全局时间片
+  char name[TASK_NAME_LEN]; // 任务名
+  u32 uid;                  // 用户 id
+  u32 pde;                  // 页目录物理地址
+  struct bitmap_t *vmap;    // 进程虚拟内存位图
+  u32 magic;                // 内核魔数，用于检测栈溢出
 } task_t;
 
 typedef struct task_frame_t
 {
-    u32 edi;
-    u32 esi;
-    u32 ebx;
-    u32 ebp;
-    void (*eip)(void);
+  u32 edi;
+  u32 esi;
+  u32 ebx;
+  u32 ebp;
+  void (*eip)(void);
 } task_frame_t;
 
-void task_init();
+task_t *running_task();
+void schedule();
+
+void task_yield();
+void task_block(task_t *task, list_t *blist, task_state_t state);
+void task_unblock(task_t *task);
+
+void task_sleep(u32 ms);
+void task_wakeup();
 
 #endif
--- src/kernel/task.c
+++ src/kernel/task.c
@@ -1,55 +1,215 @@
+#include <neonix/neonix.h>
 #include <neonix/task.h>
 #include <neonix/printk.h>
 #include <neonix/debug.h>
+#include <neonix/memory.h>
+#include <neonix/assert.h>
+#include <neonix/interrupt.h>
+#include <neonix/string.h>
+#include <neonix/bitmap.h>
+#include <neonix/syscall.h>
+#include <neonix/list.h>
 
-#define PAGE_SIZE 0x1000
+#define NR_TASKS 64
 
-task_t *a = (task_t *)0x1000;
-task_t *b = (task_t *)0x2000;
-
+extern u32 volatile jiffies;
+extern u32 jiffy;
+extern bitmap_t kernel_map;
 extern void task_switch(task_t *next);
+
+static task_t *task_table[NR_TASKS]; // 任务表
+static list_t block_list;            // 任务默认阻塞链表
+static list_t sleep_list;            // 任务睡眠链表
+
+static task_t *idle_task;
+
+// 从 task_table 里获得一个空闲的任务
+static task_t *get_free_task()
+{
+  for (size_t i = 0; i < NR_TASKS; i++)
+  {
+    if (task_table[i] == NULL)
+    {
+      task_table[i] = (task_t *)alloc_kpage(1); // todo free_kpage
+      return task_table[i];
+    }
+  }
+  panic("No more tasks");
+}
+
+// 从任务数组中查找某种状态的任务，自己除外
+static task_t *task_search(task_state_t state)
+{
+  assert(!get_interrupt_state());
+  task_t *task = NULL;
+  task_t *current = running_task();
+
+  for (size_t i = 0; i < NR_TASKS; i++)
+  {
+    task_t *ptr = task_table[i];
+    if (ptr == NULL)
+      continue;
+
+    if (ptr->state != state)
+      continue;
+    if (current == ptr)
+      continue;
+    // linux里用的是CFS，为了简单起见，这里取剩余时间最短的任务和最久未执行的任务
+    if (task == NULL || task->ticks < ptr->ticks || ptr->jiffies < task->jiffies)
+      task = ptr;
+  }
+
+  if (task == NULL && state == TASK_READY)
+  {
+    task = idle_task;
+  }
+
+  return task;
+}
+
+void task_yield()
+{
+  schedule();
+}
+
+// 任务阻塞
+void task_block(task_t *task, list_t *blist, task_state_t state)
+{
+  assert(!get_interrupt_state());
+  assert(task->node.next == NULL);
+  assert(task->node.prev == NULL);
+
+  if (blist == NULL)
+  {
+    blist = &block_list;
+  }
+
+  list_push(blist, &task->node);
+
+  assert(state != TASK_READY && state != TASK_RUNNING);
+
+  task->state = state;
+
+  task_t *current = running_task();
+  if (current == task)
+  {
+    schedule();
+  }
+}
+
+// 解除任务阻塞
+void task_unblock(task_t *task)
+{
+  assert(!get_interrupt_state());
+
+  list_remove(&task->node);
+
+  assert(task->node.next == NULL);
+  assert(task->node.prev == NULL);
+
+  task->state = TASK_READY;
+}
+
+void task_sleep(u32 ms)
+{
+  assert(!get_interrupt_state()); // 不可中断
+
+  u32 ticks = ms / jiffy;        // 需要睡眠的时间片
+  ticks = ticks > 0 ? ticks : 1; // 至少休眠一个时间片
+
+  // 记录目标全局时间片，在那个时刻需要唤醒任务
+  task_t *current = running_task();
+  current->ticks = jiffies + ticks;
+
+  // 从睡眠链表找到第一个比当前任务唤醒时间点更晚的任务，进行插入排序
+  list_t *list = &sleep_list;
+  list_node_t *anchor = &list->tail;
+
+  for (list_node_t *ptr = list->head.next; ptr != &list->tail; ptr = ptr->next)
+  {
+    task_t *task = element_entry(task_t, node, ptr);
+
+    if (task->ticks > current->ticks)
+    {
+      anchor = ptr;
+      break;
+    }
+  }
+
+  assert(current->node.next == NULL);
+  assert(current->node.prev == NULL);
+
+  // 插入链表
+  list_insert_before(anchor, &current->node);
+
+  // 阻塞状态是睡眠
+  current->state = TASK_SLEEPING;
+
+  // 调度执行其他任务
+  schedule();
+}
+
+void task_wakeup()
+{
+  assert(!get_interrupt_state()); // 不可中断
+
+  // 从睡眠链表中找到 ticks 小于等于 jiffies 的任务，恢复执行
+  list_t *list = &sleep_list;
+  for (list_node_t *ptr = list->head.next; ptr != &list->tail;)
+  {
+    task_t *task = element_entry(task_t, node, ptr);
+    if (task->ticks > jiffies)
+    {
+      break;
+    }
+
+    // unblock 会将指针清空
+    ptr = ptr->next;
+
+    task->ticks = 0;
+    task_unblock(task);
+  }
+}
 
 task_t *running_task()
 {
-  // movl 4字节
-  // movw 2字节
-  // movb 1字节
   asm volatile(
       "movl %esp, %eax\n"
-      "andl $0xfffff000, %eax\n");  // %eax的内容是函数返回值,取出结构体首地址
+      "andl $0xfffff000, %eax\n");
 }
 
 void schedule()
 {
+  assert(!get_interrupt_state()); // 不可中断
+
   task_t *current = running_task();
-  task_t *next = current == a ? b : a;
+  task_t *next = task_search(TASK_READY);
+
+  assert(next != NULL);
+  assert(next->magic == NEONIX_MAGIC);
+
+  if (current->state == TASK_RUNNING)
+  {
+    current->state = TASK_READY;
+  }
+
+  if (!current->ticks)
+  {
+    current->ticks = current->priority;
+  }
+
+  next->state = TASK_RUNNING;
+  if (next == current)
+    return;
+
   task_switch(next);
 }
 
-u32 _ofp thread_a() // 省略函数的栈帧,减少切换开销,但是无法追溯函数调用栈
+static task_t *task_create(target_t target, const char *name, u32 priority, u32 uid)
 {
-  asm volatile("sti\n");
+  task_t *task = get_free_task();
+  memset(task, 0, PAGE_SIZE);
 
-  while (true)
-  {
-    printk("A");
-    //schedule();
-  }
-}
-
-u32 _ofp thread_b()
-{
-  asm volatile("sti\n");
-
-  while (true)
-  {
-    printk("B");
-    //schedule();
-  }
-}
-
-static void task_create(task_t *task, target_t target)
-{
   u32 stack = (u32)task + PAGE_SIZE;
 
   stack -= sizeof(task_frame_t);
@@ -60,12 +220,42 @@ static void task_create(task_t *task, target_t target)
   frame->ebp = 0x44444444;
   frame->eip = (void *)target;
 
+  strcpy((char *)task->name, name);
+
   task->stack = (u32 *)stack;
+  task->priority = priority;
+  task->ticks = task->priority;
+  task->jiffies = 0;
+  task->state = TASK_READY;
+  task->uid = uid;
+  task->vmap = &kernel_map;
+  task->pde = KERNEL_PAGE_DIR; // page directory entry
+  task->magic = NEONIX_MAGIC;
+
+  return task;
 }
+
+static void task_setup()
+{
+  task_t *task = running_task();
+  task->magic = NEONIX_MAGIC;
+  task->ticks = 1;
+
+  memset(task_table, 0, sizeof(task_table));
+}
+
+extern void idle_thread();
+extern void init_thread();
+extern void test_thread();
 
 void task_init()
 {
-  task_create(a, thread_a);
-  task_create(b, thread_b);
-  schedule();
+  list_init(&block_list);
+  list_init(&sleep_list);
+
+  task_setup();
+
+  idle_task = task_create(idle_thread, "idle", 1, KERNEL_USER);
+  task_create(init_thread, "init", 5, NORMAL_USER);
+  task_create(test_thread, "test", 5, KERNEL_USER);
 }
