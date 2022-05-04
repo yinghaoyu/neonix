8f70a5b623a350408918cebf1f5403a2b55e41d9
rain <20520@126.com>
2022-05-04 12:51:10 +0800

    add syscall

---
+++ src/include/neonix/syscall.h
@@ -0,0 +1,17 @@
+#ifndef NEONIX_SYSCALL_H
+#define NEONIX_SYSCALL_H
+
+#include <neonix/types.h>
+
+typedef enum syscall_t
+{
+  SYS_NR_TEST,
+  SYS_NR_SLEEP,
+  SYS_NR_YIELD,
+} syscall_t;
+
+u32 test();
+void yield();
+void sleep(u32 ms);
+
+#endif
---
+++ src/kernel/gate.c
@@ -0,0 +1,58 @@
+#include <neonix/interrupt.h>
+#include <neonix/assert.h>
+#include <neonix/debug.h>
+#include <neonix/syscall.h>
+#include <neonix/task.h>
+
+#define LOGK(fmt, args...) DEBUGK(fmt, ##args)
+
+#define SYSCALL_SIZE 64
+
+handler_t syscall_table[SYSCALL_SIZE];
+
+void syscall_check(u32 nr)
+{
+  if (nr >= SYSCALL_SIZE)
+  {
+    panic("syscall nr error!!!");
+  }
+}
+
+static void sys_default()
+{
+  panic("syscall not implemented!!!");
+}
+
+task_t *task = NULL;
+
+static u32 sys_test()
+{
+  // LOGK("syscall test...\n");
+
+  if (!task)
+  {
+    task = running_task();
+    // LOGK("block task 0x%p \n", task);
+    task_block(task, NULL, TASK_BLOCKED);
+  }
+  else
+  {
+    task_unblock(task);
+    // LOGK("unblock task 0x%p \n", task);
+    task = NULL;
+  }
+
+  return 255;
+}
+
+void syscall_init()
+{
+  for (size_t i = 0; i < SYSCALL_SIZE; i++)
+  {
+    syscall_table[i] = sys_default;
+  }
+
+  syscall_table[SYS_NR_TEST] = sys_test;
+  syscall_table[SYS_NR_SLEEP] = task_sleep;
+  syscall_table[SYS_NR_YIELD] = task_yield;
+}
