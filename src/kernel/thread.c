1e92e72ebd62328bfc45cb1c26eeec2b3c5221cc
rain <20520@126.com>
2023-03-27 15:31:02 +0800

    add syscall waitpid

--- src/kernel/thread.c
+++ src/kernel/thread.c
@@ -32,22 +32,23 @@ void user_init_thread()
 {
   u32 counter = 0;
 
+  int status;
   while (true)
   {
-    // printf("init thread %d %d %d...\n", getpid(), getppid(), counter++);
     pid_t pid = fork();
 
     if (pid)
     {
       printf("fork after parent %d, %d, %d\n", pid, getpid(), getppid());
+      pid_t child = waitpid(pid, &status);
+      printf("wait pid %d status %d %d\n", child, status, counter++);
     }
     else
     {
       printf("fork after child %d, %d, %d\n", pid, getpid(), getppid());
+      sleep(1000);
       exit(0);
     }
-    sleep(1000);
-    // printf("task is in user mode %d\n", counter++);
   }
 }
 
