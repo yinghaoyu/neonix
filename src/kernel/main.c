703d28930b27c1481fd70d3e825f6c333d8b3e38
rain <20520@126.com>
2023-03-28 09:01:10 +0800

    add sync PIO

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -11,6 +11,7 @@ extern void clock_init();
 extern void time_init();
 extern void rtc_init();
 extern void keyboard_init();
+extern void ide_init();
 extern void task_init();
 extern void syscall_init();
 extern void hang();
@@ -27,6 +28,7 @@ void kernel_init()
   keyboard_init();
   // time_init();
   // rtc_init();
+  ide_init();
   task_init();
   syscall_init();
   set_interrupt_state(true);
--- src/kernel/thread.c
+++ src/kernel/thread.c
@@ -33,20 +33,7 @@ void user_init_thread()
   int status;
   while (true)
   {
-    pid_t pid = fork();
-
-    if (pid)
-    {
-      printf("fork after parent %d, %d, %d\n", pid, getpid(), getppid());
-      pid_t child = waitpid(pid, &status);
-      printf("wait pid %d status %d %d\n", child, status, time());
-    }
-    else
-    {
-      printf("fork after child %d, %d, %d\n", pid, getpid(), getppid());
-      sleep(1000);
-      exit(0);
-    }
+    sleep(1000);
   }
 }
 
