bfa2ff63c5ba907a4cfab2fa43a0e6ab7d52c104
rain <20520@126.com>
2023-04-02 14:31:23 +0800

    add serial

--- src/builtin/neosh.c
+++ src/builtin/neosh.c
@@ -57,28 +57,10 @@ void builtin_logo()
 
 void builtin_test(int argc, char *argv[])
 {
-  u32 status;
-
-  int *counter = (int *) mmap(0, sizeof(int), PROT_WRITE, MAP_SHARED, EOF, 0);
-  pid_t pid = fork();
-
-  if (pid)
+  printf("osh test starting...\n");
+  while (true)
   {
-    // pid_t child = waitpid(pid, &status);
-    // printf("wait pid %d status %d %d\n", child, status, time());
-    while (true)
-    {
-      (*counter)++;
-      sleep(300);
-    }
-  }
-  else
-  {
-    while (true)
-    {
-      printf("counter %d\n", *counter);
-      sleep(100);
-    }
+    test();
   }
 }
 
